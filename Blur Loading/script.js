e6613edffa5640d3565da381a0d6734be67a9ad3
Kunal Raikwar <kunalraikwar12367@gmail.com>
2026-02-19 04:38:01 +0530

    blur loading

--- Blur Loading/script.js
+++ Blur Loading/script.js
@@ -11,7 +11,6 @@ const blurring = () => {
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 };
 
-// For reference: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
